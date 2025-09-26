f69d00b376ee579a3e14ab3d1d196bc31cef066e
Abhinav7864 <ak1492612@gmail.com>
2025-09-26 21:20:00 +0530

    Added Day 3 code : Remove Duplicates

---
+++ Remove Duplicates from Sorted Array Day 3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+// Corrected line: nums[++j] = nums[i];
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty()) return 0;
+        int j = 0;
+        for(int i = 1; i < nums.size(); i++){
+            if (nums[i] != nums[j]) {
+                nums[++j] = nums[i];
+            }
+        }
+        return j + 1;
+    }
+};
