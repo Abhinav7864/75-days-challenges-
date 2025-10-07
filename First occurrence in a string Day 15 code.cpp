98c219786c689f533766ee44454c327cc80843ab
Abhinav7864 <ak1492612@gmail.com>
2025-10-07 23:14:57 +0530

    The code for Day 15

---
+++ First occurrence in a string Day 15 code.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;    
+class Solution {
+public:
+    int strStr(std::string haystack, std::string needle) {
+        for (int i = 0; i <= haystack.length() - needle.length(); ++i) {
+            if (haystack.substr(i, needle.length()) == needle) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
